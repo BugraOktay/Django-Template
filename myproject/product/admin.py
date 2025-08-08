e8eb800d7ec9a2425a223a5c82f160d116ee954f
BugraOktay <bugraoktayates@gmail.com>
2025-08-08 17:58:56 +0300

    xd

--- myproject/product/admin.py
+++ myproject/product/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Product
 # Register your models here.
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'img')
+    search_fields = ('name', 'description')
+
+admin.site.register(Product, ProductAdmin)
+
